5338c14bb51ba26553455ca4ccdbc2a0db2e5284
AshTappin <ashtappin1592@hotmail.com>
2017-08-16 22:54:51 +0100

    Tidying up messages

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,6 +1,5 @@
 const loki	 = require('lokijs');
 const db = new loki("Messagerary")
-console.log("hello!")
 const messages = db.addCollection('messages');
 
 module.exports = {
@@ -10,14 +9,14 @@ module.exports = {
 		var messageInserted = messages.insert({id: 2344, message: `${req.body}`});
 		console.dir(messageInserted);
 
-		res.send({ id: messageInserted.$loki, message: req.body});
+		res.send({ id: messageInserted.$loki});
 	},
 
 	getMessage : function (req, res) {
 		console.log(`Getting message with ID [${req.params.message_id}]`)
 		const messageRecord = messages.get(parseInt(req.params.message_id));
 		console.dir(messageRecord);		
-		res.send({ message: messageRecord.message});
+		res.send(messageRecord.message);
 	}
 
 }
