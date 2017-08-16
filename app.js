565e2e8ac39c340609509c8b7661a30a87f67abb
Ash Tappin <at@phoebussoftware.com>
2017-08-16 16:03:32 +0100

    Starting to add lokijs support

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const loki	 = require('lokijs');
+const db = new loki("Messagerary")
+const messages = db.addCollection('messages');
+
+messages.insert({message: "HeyHey"});
+
+var message = messages.find({message: "HeyHey"});
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-mongoose.connect('mongodb://localhost/messagery');
-mongoose.connection.on('open', () => console.log('Mongoose connected.'));
-
 var server = app.listen(app.get('port'), function() {
 	console.log(`Server up: http://localhost:${app.get('port')}`);
-});
