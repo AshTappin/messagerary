8573d26cc5c1e379530494016e1cd977502ac881
AshTappin <ashtappin1592@hotmail.com>
2017-08-20 22:21:00 +0100

    Refactored test to use request-promise

--- app.spec.js
+++ app.spec.js
@@ -1,31 +1,45 @@
 const request = require("request");
 const app = require("./app.js")
-
+const requestPromise = require("request-promise")
+const baseUrl = "http://localhost:3000/messagerary";
 
 describe("Messagerary Service", function() {
-	const baseUrl = "http://localhost:3000/messagerary";
 
-		it("allows a message to be stored and read back", function(done) {
+		it("allows a message to be stored and read back", (done) => {
 
-			 request.post({
+			var id = requestPromise({
+				method: "POST",
 			 	url:     `${baseUrl}/messages/`,
   	 			body:    "T o p  S e c r e t  M e s s a g e",
-	 			headers: {'Content-Type': 'text/plain'}
-			 }, function(error, response, body) {
-			 	expect(response.statusCode).toEqual(200);
-			 	console.dir(body);
-			  	const messageId = body.id;
-
-			  	request.get(`${baseUrl}/messages/${messageId}`, function(error, response, body) {
-			  		expect(response.statusCode).toEqual(200);
-				  	expect(body).toBe("T o p  S e c r e t  M e s s a g e")
-				  	app.stopServer();
-			 		done();
-				 });
-			 	
-			 	
-			 });
+	 			headers: {'Content-Type': 'text/plain'},
+	 			json: true})
+			.then((response) => response.id)
+			.then(getMessageFromWebServiceWithID())
+			.then(checkMessageReturnedIs("T o p  S e c r e t  M e s s a g e"))
+			.then(() => stopServer(done))
+			.catch(error => {
+				stopServer(done);
+			});
 		});
-	
 
 });
+
+function getMessageFromWebServiceWithID() {
+	return (id) => getMessage(id);
+}
+
+function getMessage(messageId) {
+
+	return requestPromise(`${baseUrl}/messages/${messageId}`)
+		.then((response) => response.replace (/(^")|("$)/g, ''))
+		.catch((error)=> console.log(error));
+}
+
+function checkMessageReturnedIs(expectedMessage) {
+	return message => expect(message).toBe(expectedMessage);
+}
+
+function stopServer(done) {
+	app.stopServer();
+	done();
+}
